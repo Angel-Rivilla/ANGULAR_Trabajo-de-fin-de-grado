a7e8721cf7b5bf435420dc750e951aeb8360f53d
Angel Rivilla <angelrivillaarre@gmail.com>
2021-08-07 14:57:33 +0200

    commit

--- frontend/src/assets/js/details-product.js
+++ frontend/src/assets/js/details-product.js
@@ -1,17 +1,26 @@
 const fila2 = document.querySelector('.home-description-scroll');
-
+const scrollX= document.querySelector('.mat-drawer-content');
 const flechaStatica = document.getElementById('fixedDescription');
 const flechaMid = document.getElementById('scrollMidInformation');
 const flechaEnd = document.getElementById('scrollEndComentarios');
     
 flechaStatica.addEventListener('click', () => {
     fila2.scrollLeft = 0;
+    if(scrollX.scrollTop > 930){
+        scrollX.scrollTop = 930;
+    }
 });
 
 flechaMid.addEventListener('click', () => {
     fila2.scrollLeft = 833.3333129882812;
+    if(scrollX.scrollTop > 930){
+        scrollX.scrollTop = 930;
+    }
 });
 
 flechaEnd.addEventListener('click', () => {
     fila2.scrollLeft = 1666,666625976562;
+    if(scrollX.scrollTop > 930){
+        scrollX.scrollTop = 930;
+    }
 });
